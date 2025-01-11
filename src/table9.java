4c24741dc89469a73fb45b41a343cf32b3b3d7ac
AL5130 <jprathap909@gmail.com>
2025-01-11 12:30:59 +0530

    first comit

---
+++ src/table9.java
@@ -0,0 +1,28 @@
+public class table9 {
+    public static void main(String[] args) {
+        int a= 9;
+    System.out.printf("9 x 1 = %d",a);
+        System.out.println();
+    System.out.printf("9 x 2 = %d",a*2);
+        System.out.println();
+    System.out.printf("9 x 3 = %d",a*3);
+        System.out.println();
+    System.out.printf("9 x 4 = %d",a*4);
+        System.out.println();
+    System.out.printf("9 x 5 = %d",a*5);
+        System.out.println();
+    System.out.printf("9 x 6 = %d",a*6);
+        System.out.println();
+    System.out.printf("9 x 7 = %d",a*7);
+        System.out.println();
+    System.out.printf("9 x 8 = %d",a*8);
+        System.out.println();
+    System.out.printf("9 x 9 = %d",a*9);
+        System.out.println();
+    System.out.printf("9 x 10 = %d",a*10);
+        System.out.println();
+    System.out.printf("9 x 11= %d",a*11);
+        System.out.println();
+    System.out.printf("9 x 12 = %d",a*12);
+        System.out.println();
+    }}
