c3dfccfd36d58eab0a1ca1c319bf151a320d0361
AL5130 <jprathap909@gmail.com>
2025-01-11 22:40:58 +0530

    rev01

--- src/table9.java
+++ src/table9.java
@@ -1,28 +1,31 @@
 public class table9 {
     public static void main(String[] args) {
         int a= 9;
-    System.out.printf("9 x 1 = %d",a);
-        System.out.println();
-    System.out.printf("9 x 2 = %d",a*2);
-        System.out.println();
-    System.out.printf("9 x 3 = %d",a*3);
-        System.out.println();
-    System.out.printf("9 x 4 = %d",a*4);
-        System.out.println();
-    System.out.printf("9 x 5 = %d",a*5);
-        System.out.println();
-    System.out.printf("9 x 6 = %d",a*6);
-        System.out.println();
-    System.out.printf("9 x 7 = %d",a*7);
-        System.out.println();
-    System.out.printf("9 x 8 = %d",a*8);
-        System.out.println();
-    System.out.printf("9 x 9 = %d",a*9);
-        System.out.println();
+    System.out.printf("%d x 1 = %d%n",a,a);
+//        System.out.println();
+    System.out.printf("%d x 2 = %d%n",a,a*2);
+//        System.out.println();
+    System.out.printf("%d x 3 = %d%n",a,a*3);
+//        System.out.println();
+    System.out.printf("%d x 4 = %d%n",a,a*4);
+//        System.out.println();
+    System.out.printf("%d x 5 = %d%n",a,a*5);
+//        System.out.println();
+    System.out.printf("%d x 6 = %d%n",a,a*6);
+//        System.out.println();
+    System.out.printf("9%d x 7 = %d%n",a,a*7);
+//        System.out.println();
+    System.out.printf("%d x 5 = %d%n",a,a*8);
+//        System.out.println();
+    System.out.printf("%d x 5 = %d%n",a,a*9);
+//        System.out.println();
     System.out.printf("9 x 10 = %d",a*10);
         System.out.println();
     System.out.printf("9 x 11= %d",a*11);
         System.out.println();
     System.out.printf("9 x 12 = %d",a*12);
         System.out.println();
-    }}
+    }
+
+    }
+
